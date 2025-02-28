bdc7da1a06b07a814968f2e4d524ba4e68db12be
laristizabal02 <lauris.paz@hotmail.es>
2025-02-27 21:38:19 -0700

    fixing server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,53 +1,58 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
-import path from 'node:path';
-//import type { Request, Response } from 'express';
 import { expressMiddleware } from '@apollo/server/express4';
 // import path from 'path';
-//import cors from 'cors';
+import cors from 'cors';
 import { authenticateToken } from './services/auth.js';
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 
-
 // import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-const app = express();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
-  await db;
 
-  app.use(express.urlencoded({ extended: false }));
+  const corsOptions = {
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000', // Allow requests from the React frontend
+    credentials: true, // Include credentials like cookies or headers
+  };
+
+  app.use(cors(corsOptions)); // Apply CORS middleware
+  
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-
+  
   app.use('/graphql', expressMiddleware(server as any,
     {
       context: authenticateToken as any
     }
   ));
 
+  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static('../client/dist'));
 
-    app.get('*', (_req , res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
+    app.get('*', (_req, res) => {
+    res.sendFile('../client/dist/index.html');
+  });
+}
+  
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
-
 };
 
-// Call the async function to start the server
 startApolloServer();
